19ddfedcbe5d73fe00c72f8db6cb75c345ce95b2
h1101221017 <h1101221017@student.untan.ac.id>
2024-12-04 01:25:59 +0000

    halaman search

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -71,7 +71,7 @@ class ProfileView extends StatelessWidget {
               physics: const NeverScrollableScrollPhysics(),
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3,
-                crossAxisSpacing: 2,
+                crossAxisSpacing: 3,
                 mainAxisSpacing: 1,
               ),
               itemCount: user.images.length,
